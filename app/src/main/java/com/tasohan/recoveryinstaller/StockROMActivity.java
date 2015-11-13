6aadc76c9f6532d1377bc4eae8ae4128282ab3ed
Avinaba Dalal <d97.avinaba@gmail.com>
2015-11-13 13:01:44 +0530

    Implement file checking mechanism
    For files necessary for recovery installation in stock.

--- app/src/main/java/com/tasohan/recoveryinstaller/StockROMActivity.java
+++ app/src/main/java/com/tasohan/recoveryinstaller/StockROMActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import java.io.File;
+
 /**
  * Created by Avinaba on 11/12/2015.
  */
@@ -15,9 +17,21 @@ public class StockROMActivity extends AppCompatActivity {
     }
 
 
-    public void put_details() {
+    public boolean file_checklist_core (String filename) {
+        File file = new File(filename);
+        if(file.exists())
+            return true;
+        else
+            return false;
+    }
 
-
+    public boolean file_checklist () {
+        String[] files = {"sdcard/busybox","sdcard/e2fsck.sh","sdcard/recovery.sh","sdcard/step3.sh"};
+        for(String file:files) {
+            if(!file_checklist_core(file))
+               return false;
+        }
+        return true;
     }
 
 }
