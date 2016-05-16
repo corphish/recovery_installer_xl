60eebf7d95df2d37089d1747789dc75de8489bc2
Avinaba Dalal <d97.avinaba@gmail.com>
2016-05-16 19:06:10 +0530

    MainActivity: Cleanup

--- app/src/main/java/com/tasohan/recoveryinstaller/MainActivity.java
+++ app/src/main/java/com/tasohan/recoveryinstaller/MainActivity.java
@@ -1,38 +1,46 @@
 package com.tasohan.recoveryinstaller;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.FragmentTransaction;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.DialogPreference;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.app.ActionBar;
+import android.util.Log;
 
 /**
  * Created by Avinaba on 11/12/2015.
  */
 public class MainActivity extends Activity {
 
-
-    public int act = 0;
+    public String LOG_TAG = "RecoveryInstaller";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
+        Intent intent;
         if(Build.VERSION.RELEASE.contains("4.1") || Build.VERSION.RELEASE.contains("4.2")) {
-            Intent intent = new Intent(this,StockROMActivity.class);
-            act = 1;
-            startActivity(intent);
+            intent = new Intent(this,StockROMActivity.class);
         } else {
-            Intent intent = new Intent(this,CustomROMActivity.class);
-            act = 2;
-            startActivity(intent);
+            intent = new Intent(this,CustomROMActivity.class);
         }
+        startActivity(intent);
 
 
         finish();
     }
+
+
 }
